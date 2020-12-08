1d6c9583bfd9d6c24c67130cecd6991f6c9589d1
Rick Helmus <r.helmus@uva.nl>
2020-12-08 18:54:59 +0100

    more qualities

--- R/generics.R
+++ R/generics.R
@@ -21,7 +21,7 @@ setGeneric("comparison", function(..., groupAlgo,
 setGeneric("groupFeatures", function(feat, algorithm, ...) standardGeneric("groupFeatures"))
 setGeneric("groupFeaturesScreening", function(fGroups, scr) standardGeneric("groupFeaturesScreening"))
 setGeneric("replicateGroupSubtract", function(fGroups, rGroups, threshold = 0) standardGeneric("replicateGroupSubtract"))
-setGeneric("calculatePeakQualities", function(fGroups) standardGeneric("calculatePeakQualities"))
+setGeneric("calculatePeakQualities", function(fGroups, flatnessFactor = 0.05) standardGeneric("calculatePeakQualities"))
 
 ### utils (XCMS)
 
